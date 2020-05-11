bfbfaa08bd17dea5b45dde45b2fad94b719003b9
Tommi Gustafsson <tommi.gustafsson@hyvanmielenpelit.fi>
2020-05-11 10:04:19 +0300

    Added color transparency option and did some cleaning of code.

--- TileSetCompiler/MonsterCompiler.cs
+++ TileSetCompiler/MonsterCompiler.cs
@@ -109,95 +109,24 @@ namespace TileSetCompiler
                 var sourceRelativePath = Path.Combine(_subDirName, sourceSubDir2, sourceFileName);
                 FileInfo sourceFile = new FileInfo(Path.Combine(sourceMonsterDirPath, sourceFileName));
 
-                var destDir = Program.StatueOutputDirectory ?? Program.WorkingDirectory;
                 var destSubDirPath = Path.Combine(_statueDirName, name.ToLower());
                 string destFileName = _type_statue + "_" + name.ToLower() + genderSuffix + Program.ImageFileExtension;
                 var destFileRelativePath = Path.Combine(destSubDirPath, destFileName);
 
-                string destDirPath = null;
-                DirectoryInfo destSubDir = null;
-                FileInfo destFile = null;
-
-                if (Program.CreateStatuesOnDisk)
+                bool isUnknown;
+                using (var image = StatueCreator.CreateStatueBitmapFromFile(sourceFile, out isUnknown))
                 {
-                    destDirPath = Path.Combine(destDir.FullName, destSubDirPath);
-                    destSubDir = new DirectoryInfo(destDirPath);
-                    if (!destSubDir.Exists)
+                    if(!isUnknown)
                     {
-                        try
-                        {
-                            destSubDir.Create();
-                        }
-                        catch (Exception ex)
-                        {
-                            throw new Exception(string.Format("Could not create statue sub directory '{0}'.", destSubDir.FullName), ex);
-                        }
-                    }
-                    destFile = new FileInfo(Path.Combine(destSubDir.FullName, destFileName));
-
-                    if (destFile.Exists)
-                    {
-                        try
-                        {
-                            destFile.Decrypt();
-                        }
-                        catch (Exception ex)
-                        {
-                            throw new Exception(string.Format("Could not delete statue file '{0}'.", destFile.FullName), ex);
-                        }
-                    }
-
-                    bool isUnknown = false;
-                    StatueCreator.CreateStatue(sourceFile, destFile, out isUnknown);
-
-                    if(isUnknown)
-                    {
-                        Console.WriteLine("Monster statue file '{0}' not found. Using Unknown Monster Statue icon.", sourceFile.FullName);
+                        WriteTileNameStatueSuccess(sourceRelativePath, destFileRelativePath);
                     }
                     else
                     {
-                        Console.WriteLine("Created statue from '{0}' to '{1}'.", sourceFile.FullName, destFile.FullName);
+                        Console.WriteLine("Monster file '{0}' not found for statue creation. Using Unknown Monster Statue icon.", sourceFile.FullName);
+                        WriteTileNameStatueError(sourceRelativePath, destFileRelativePath);
                     }
-                }
-
-                if(Program.ReadStatuesFromDisk)
-                {
-                    FileInfo usedStatueFile = destFile;
-                    if(destFile.Exists)
-                    {
-                        WriteTileNameStatueSuccessDisk(sourceRelativePath, destFileRelativePath);
-                    }
-                    else
-                    {
-                        Console.WriteLine("Monster statue file '{0}' not found. Using Unknown Monster Statue icon.", destFile.FullName);
-                        usedStatueFile = UnknownStatueFile;
-                        WriteTileNameErrorFileNotFound(sourceRelativePath, "Using Unknown Monster Statue icon");
-                    }
-                    using (var image = new Bitmap(Image.FromFile(usedStatueFile.FullName)))
-                    {
-                        DrawImageToTileSet(image);
-                        IncreaseCurXY();
-                    }
-                }
-
-                if(!Program.CreateStatuesOnDisk && !Program.ReadStatuesFromDisk)
-                {
-                    //Create and read to memory
-                    bool isUnknown;
-                    using (var image = StatueCreator.CreateStatueBitmapFromFile(sourceFile, out isUnknown))
-                    {
-                        if(!isUnknown)
-                        {
-                            WriteTileNameStatueSuccessMemory(sourceRelativePath, destFileRelativePath);
-                        }
-                        else
-                        {
-                            Console.WriteLine("Monster statue file '{0}' not found. Using Unknown Monster Statue icon.", sourceFile.FullName);
-                            WriteTileNameErrorFileNotFound(sourceRelativePath, "Using Unknown Monster Statue icon");
-                        }
-                        DrawImageToTileSet(image);
-                        IncreaseCurXY();
-                    }
+                    DrawImageToTileSet(image);
+                    IncreaseCurXY();
                 }
             }
         }        
